201201927be996bc808f57f9cf8cd7ee761e6e30
ako <a.veysinejad@gmail.com>
2024-04-22 11:29:26 +0330

    feat: added brand colors

--- src/config/theme.tsx
+++ src/config/theme.tsx
@@ -2,6 +2,21 @@
 
 import { extendTheme } from "@chakra-ui/react";
 
-const theme = extendTheme({});
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: "#f0f4ff",
+      100: "#f7fafc",
+      200: "#edf2f7",
+      300: "#e2e8f0",
+      400: "#cbd5e0",
+      500: "#a0aec0",
+      600: "#718096",
+      700: "#4a5568",
+      800: "#2d3748",
+      900: "#1a202c",
+    },
+  },
+});
 
 export default theme;
